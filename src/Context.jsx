21d38ebe5d7b78a95bbe575ce412f0d89693c562
Siphamandla <siphabili@gmail.com>
2024-02-06 23:59:40 +0200

    dark mode user preferrence

--- src/Context.jsx
+++ src/Context.jsx
@@ -2,23 +2,26 @@ import { createContext,useContext,useState,useEffect } from "react";
 
 const context = createContext();
 
-export const AppProvider =({children})=>{
-    const [isDarkTHeme,setDarkTheme] = useState(false);
-    const [input,setInput] = useState('cat');
-   
-
+const onloadTheme = () =>{
+    const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     
+    return prefersDarkMode
+}
 
+export const AppProvider =({children})=>{
+    const [isDarkTHeme,setDarkTheme] = useState(onloadTheme());
+    const [input,setInput] = useState('cat');
+    
     const toggleDarkTheme =()=>{
         const newDarkTheme = !isDarkTHeme
         setDarkTheme(newDarkTheme);
         const body = document.querySelector("body");
         body.classList.toggle('dark-theme',newDarkTheme);
         console.log(isDarkTHeme)
-        console.log(body)
-       
+        console.log(body) 
     }
 
+    useEffect(()=>{ document.body.classList.toggle('dark-theme',isDarkTHeme)},[])
     return <context.Provider value={{isDarkTHeme,setDarkTheme,toggleDarkTheme,input,setInput}}>
         {children}
     </context.Provider>
